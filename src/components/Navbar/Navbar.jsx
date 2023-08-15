d85dd3d07b2b3c3a833fe9f2df76593a22ef5d3b
shahab-edraak <shahab@edraaksystems.com>
2023-08-15 16:03:28 +0500

    navbar initial and responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,26 +2,52 @@ import React from "react";
 import "./Navbar.css";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { BiGridAlt } from "react-icons/bi";
+import { BsSearch } from "react-icons/bs";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { AiOutlineMail } from "react-icons/ai";
+import { IoMdNotificationsOutline } from "react-icons/io";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navbar_logo_info">
-        <span className="nav_hamburger">
+        <div className="nav_hamburger">
           <GiHamburgerMenu />
-        </span>
-        <span className="logo_wrapper">
-          <span className="logo_img">
+        </div>
+        <div className="logo_wrapper">
+          <div className="logo_img">
             <img src={require("../../assets/logo.png")} alt="logo" />
-          </span>
-          <h2>LMS</h2>
-        </span>
-        <span className="nav_category">
+          </div>
+          <span className="logo_name">LMS</span>
+        </div>
+        <div className="nav_category">
           <BiGridAlt />
+        </div>
+      </div>
+      <div className="navbar_search">
+        <span className="search_logo">
+          <BsSearch />
+        </span>
+        <input
+          type="text"
+          placeholder="Search for Tuts Videos, Tutors, Tests and more.."
+        />
+      </div>
+      <div className="navbar_user_info">
+        <button className="user_btn">Create New Course</button>
+        <span className="user_info_notification">
+          <AiOutlineShoppingCart />
+        </span>
+        <span className="user_info_notification">
+          <AiOutlineMail />
+        </span>
+        <span className="user_info_notification">
+          <IoMdNotificationsOutline />
+        </span>
+        <span className="user_dp">
+          <img src={require("../../assets/hd_dp.jpg")} alt="hd_dp" />
         </span>
       </div>
-      <div className="navbar_search"></div>
-      <div className="navbar_user_info"></div>
     </div>
   );
 };
