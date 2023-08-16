ccf447d8e9b112f482a88cf24087ddbcf181d419
shahab-edraak <shahab@edraaksystems.com>
2023-08-16 17:46:50 +0500

    featured courses

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,6 +3,7 @@ import "./Home.css";
 import { StreamData } from "../Stream/StreamData";
 import { GrFormNext } from "react-icons/gr";
 import { GrFormPrevious } from "react-icons/gr";
+import { CiMenuKebab } from "react-icons/ci";
 
 const Home = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -22,7 +23,7 @@ const Home = () => {
     <div className="home_page">
       <section className="home_page_left">
         <div className="home_page_left_live_stream_container">
-          <div className="home_page_left_live_stream_heading">
+          <div className="home_page_left_heading">
             <h4>Live Streams</h4>
             <button className="home_btn">See All</button>
           </div>
@@ -56,7 +57,30 @@ const Home = () => {
             </button>
           </div>
         </div>
-        <div className="home_page_left_"></div>
+        <div className="home_page_left_featured_container">
+          <div className="home_page_left_heading">
+            <h4>Featured Courses</h4>
+            <button className="home_btn">See All</button>
+          </div>
+          <div className="home_page_left_featured_courses">
+            <div className="featured_courses">
+              <img
+                src={require("../../assets/featured1.jpg")}
+                alt="featured1"
+              />
+              <div className="featured_courses_views">
+                <span className="course_views">109k Views.</span>
+                <span className="course_views">10 days ago</span>
+                <button className="course_share">
+                  <CiMenuKebab />
+                </button>
+              </div>
+              <h5>
+                Complete Python Bootcamp: Go from zero to hero in Python 3
+              </h5>
+            </div>
+          </div>
+        </div>
         <div className="home_page_left_"></div>
         <div className="home_page_left_"></div>
         <div className="home_page_left_"></div>
