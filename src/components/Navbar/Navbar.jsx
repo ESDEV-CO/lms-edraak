32a90b28df8bd43e428b966bcd640c48c73f8bb5
shahab-edraak <shahab@edraaksystems.com>
2023-08-16 12:58:12 +0500

    message drop down

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,6 +10,7 @@ import SideBar from "../Sidebar/SideBar";
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
+  const [isMessage, setIsMessage] = useState(false);
 
   return (
     <>
@@ -39,15 +40,18 @@ const Navbar = () => {
         </div>
         <div className="navbar_user_info">
           <button className="primary_btn user_btn">Create New Course</button>
-          <span className="user_info_notification">
+          <button className="user_info_notification">
             <AiOutlineShoppingCart />
-          </span>
-          <span className="user_info_notification">
+          </button>
+          <button
+            onClick={() => setIsMessage(!isMessage)}
+            className="user_info_notification"
+          >
             <AiOutlineMail />
-          </span>
-          <span className="user_info_notification">
+          </button>
+          <button className="user_info_notification">
             <IoMdNotificationsOutline />
-          </span>
+          </button>
           <span className="user_dp">
             <img src={require("../../assets/hd_dp.jpg")} alt="hd_dp" />
           </span>
@@ -55,6 +59,35 @@ const Navbar = () => {
       </div>
       <div>
         <div className="toggle_sidebar">{toggle && <SideBar />}</div>
+        {isMessage && (
+          <div className="message_toggle">
+            <div className="message_toggle_user">
+              <span className="toggle_user_img">
+                <img src={require("../../assets/img-6.jpg")} alt="img6" />
+              </span>
+              <span className="toggle_user_info">
+                <h6>Zoena Singh</h6>
+                <p>
+                  Hi! Sir, How are you. I ask you one thing please explain it
+                  this video price.
+                </p>
+                <span>2 min ago</span>
+              </span>
+            </div>
+            <div className="message_toggle_user">
+              <span className="toggle_user_img">
+                <img src={require("../../assets/img-5.jpg")} alt="img6" />
+              </span>
+              <span className="toggle_user_info">
+                <h6>Joy Dua</h6>
+                <p>
+                  Hello, I paid you video tutorial but did not play error 404.
+                </p>
+                <span>10 min ago</span>
+              </span>
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
