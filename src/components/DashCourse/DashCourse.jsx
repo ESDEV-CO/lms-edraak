13d5d01e8e49751995cc35cc4abe84be64db660c
shahab-edraak <shahab@edraaksystems.com>
2023-08-25 11:40:10 +0500

    dash course

--- src/components/DashCourse/DashCourse.jsx
+++ src/components/DashCourse/DashCourse.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 import "./DashCourse.css";
+import { BiBookAlt } from "react-icons/bi";
 
 const DashCourse = () => {
-  return <div>DashCourse</div>;
+  return (
+    <div className="dash_course_container">
+      <div className="container_heading">
+        <span>
+          <BiBookAlt />
+        </span>
+        <h3>Courses</h3>
+      </div>
+      <div className="course_creation_card box_shadow">
+        <span>
+          <BiBookAlt />
+        </span>
+        <h3>Jump into Course Creation</h3>
+        <button className="primary_btn">Create your Course</button>
+      </div>
+    </div>
+  );
 };
 
 export default DashCourse;
